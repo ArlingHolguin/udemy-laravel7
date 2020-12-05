4c5166ebf7f8b39e3b635e9971a9ced0d0334805
Arling Holguin <arlingholguin@gmail.com>
2020-12-04 19:30:06 -0500

    Clase 37 usando variable de session
    Co-Authored-By: ArlingHolguin <54909429+arlingholguin@users.noreply.github.com>

--- udemy_laravel/app/Http/Controllers/ProductController.php
+++ udemy_laravel/app/Http/Controllers/ProductController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    //con esta funcion  retorna la vista de todos los prodcutos 
     public function index(){
        //$products = DB::table('products')->get();       
        //return $products;
@@ -19,19 +19,33 @@ class ProductController extends Controller
         ]);
     }
 
+
+
+    // esta funcion returna a la vista create o sea formulario de crear producto 
     public function create(){
         return view('products.create');
     }
 
+
+    //recibe a Create de la vista create
     public function store(){
         //
         //dd('Estamos en store');
+        if(request()->status == 'available' && request()->stock == 0){
+            // session()->put('error', 'No puede crear un stock de cero y poner el prodcuto disponible');
+            session()->flash('error', 'No puede crear un stock de cero y poner el prodcuto disponible');
+            return redirect()->back();
+        }
+       
         $product = Product::create(request()->all());
         //return redirect()->back();
         // return redirect()->action('MainController@index');
         return redirect()->route('products.index');
     }
 
+
+
+    //recibe todos los prodcutos  del modelo Product y lo retorna en la vista Show
     public function show($product){
         // $product = DB::table('products')->where('id', $product)->get();
         // $product = DB::table('products')->find($product);
@@ -43,23 +57,31 @@ class ProductController extends Controller
             
         ]);
     }
+
+
+
+    //Esta funcion trae productos del modelo Product considerando su id 
     public function edit($product){
         return view('products.edit')->with([
             'product' => Product::findOrFail($product),
         ]);
     }
 
+
+    // Esta funcion recibe datos de la vista edit parara ser actualizados en la base de datos retornando a la vita principal  
     public function update($product){
         $product = Product::findOrFail($product);
         $product->update(request()->all());
         return redirect()->route('products.index');
     }
 
+
+
+    // Con esta funcion traemos el id de la tabla  Product y eliminamos  retornando a la vista principal 
     public function destroy($product){
         //
         $product = Product::findOrFail($product);
         $product->delete();
-
         return redirect()->route('products.index');
 
     }
